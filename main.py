ecc0482ea6a9f4057c5814f14f51d934bae3cb73
Zodi du Toit <ZodiDuToit@gmail.com>
2020-09-03 11:51:44 +0000

    turned it into a joke bot

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-import os
-from keep_alive import keep_alive
+
+import requests
 from discord.ext import commands
 
-bot = commands.Bot(
-	command_prefix="!",  # Change to desired prefix
-	case_insensitive=True  # Commands aren't case-sensitive
-)
+client = commands.Bot(command_prefix = '!')
 
-bot.author_id = 487258918465306634  # Change to your discord id!!!
+url = "https://icanhazdadjoke.com/search"
 
-@bot.event 
-async def on_ready():  # When the bot is ready
-    print("I'm in")
-    print(bot.user)  # Prints the bot's username and identifier
+@client.event
+async def on_message(message):
+	
+	if message.author != client.user:
 
+		response = requests.get(url, headers={"Accept": "application/json"},params={"term":message.content})
 
-extensions = [
-	'cogs.cog_example'  # Same name as it would be if you were importing it
-]
+		try:
+			await message.channel.send(response.json()["results"][2]["joke"])
 
-if __name__ == '__main__':  # Ensures this is the file being ran
-	for extension in extensions:
-		bot.load_extension(extension)  # Loades every extension.
+		except:
+			await message.channel.send("no such topic")
 
-keep_alive()  # Starts a webserver to be pinged.
-token = os.environ.get("DISCORD_BOT_SECRET") 
-bot.run(token)  # Starts the bot
+client.run("your token here")
